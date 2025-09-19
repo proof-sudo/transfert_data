5b9517fe37f6747937b76a865b76aaacfb8a7932
prince-rebel <djakaridjatraore1997@gmail.com>
2025-09-19 12:25:27 +0000

    3 commit

--- models/res_config_settings.py
+++ models/res_config_settings.py
@@ -8,16 +8,19 @@ class ResConfigSettings(models.TransientModel):
         help="URL du serveur Odoo cible pour synchronisation"
     )
 
+    PARAM_KEY = 'transfer_to_odoo17.external_odoo_base_url'
+
     @api.model
     def get_values(self):
         res = super(ResConfigSettings, self).get_values()
         IrConfig = self.env['ir.config_parameter'].sudo()
         res.update(
-            external_odoo_base_url=IrConfig.get_param('external_odoo.base_url', default=''),
+            external_odoo_base_url=IrConfig.get_param(self.PARAM_KEY, default=''),
         )
         return res
 
+    @api.multi
     def set_values(self):
         super(ResConfigSettings, self).set_values()
         IrConfig = self.env['ir.config_parameter'].sudo()
-        IrConfig.set_param('external_odoo.base_url', self.external_odoo_base_url or '')
+        IrConfig.set_param(self.PARAM_KEY, self.external_odoo_base_url or '')
